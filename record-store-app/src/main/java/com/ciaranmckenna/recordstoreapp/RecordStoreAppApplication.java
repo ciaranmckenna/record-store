8ddf73a82c6c30f1c4f3d4283cc8f82a19f8a0d9
Ciaran McKenna <hello@ciaranmckenna.com>
2022-04-05 14:32:37 +0100

    Adding project files

---
+++ record-store-app/src/main/java/com/ciaranmckenna/recordstoreapp/RecordStoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.ciaranmckenna.recordstoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordStoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordStoreAppApplication.class, args);
+	}
+
+}
---
+++ record-store-app/src/test/java/com/ciaranmckenna/recordstoreapp/RecordStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciaranmckenna.recordstoreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordStoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
